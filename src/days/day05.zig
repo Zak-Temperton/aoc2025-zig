64c50c8c92c928476c1cdc4878b4e33dfa697c52
Zak-Temperton <zbtemperton@gmail.com>
2025-12-05 19:05:36 +1300

    day05

--- src/days/day05.zig
+++ src/days/day05.zig
@@ -25,30 +25,119 @@ pub fn run(alloc: std.mem.Allocator, stdout: *std.io.Writer) !void {
     });
 }
 
-fn part1(alloc: Allocator, input: []const u8) !u32 {
-    _ = alloc;
-    _ = input;
-    return 0;
+const Range = struct { lower: u64, upper: u64 };
+
+fn sortRange(_: void, left: Range, right: Range) bool {
+    return left.lower < right.lower;
 }
 
-fn part2(alloc: Allocator, input: []const u8) !u32 {
-    _ = alloc;
-    _ = input;
-    return 0;
+fn createRanges(alloc: Allocator, input: []const u8, index: *usize) ![]Range {
+    var ranges = try std.ArrayList(Range).initCapacity(alloc, 32);
+    defer ranges.deinit(alloc);
+
+    while (input[index.*] != '\n') {
+        const lower = readInt(u64, input, index);
+        index.* += 1;
+        const upper = readInt(u64, input, index);
+        index.* += 1;
+        const new_range = Range{ .lower = lower, .upper = upper };
+        try ranges.append(alloc, new_range);
+    }
+
+    std.mem.sort(Range, ranges.items, {}, sortRange);
+    var new_ranges = try std.ArrayList(Range).initCapacity(alloc, 32);
+
+    for (ranges.items) |new_range| {
+        var merged = false;
+        for (new_ranges.items) |*range| {
+            if (new_range.lower <= range.lower and new_range.upper >= range.lower) {
+                range.lower = new_range.lower;
+                merged = true;
+            }
+            if (new_range.upper >= range.upper and new_range.lower <= range.upper) {
+                range.upper = new_range.upper;
+                merged = true;
+            }
+            if (new_range.lower >= range.lower and new_range.upper <= range.upper) {
+                merged = true;
+            }
+            if (merged) break;
+        }
+        if (!merged) {
+            try new_ranges.append(alloc, new_range);
+        }
+    }
+
+    return new_ranges.toOwnedSlice(alloc);
+}
+
+fn part1(alloc: Allocator, input: []const u8) !u32 {
+    var index: usize = 0;
+    const ranges = try createRanges(alloc, input, &index);
+    defer alloc.free(ranges);
+    var fresh: u32 = 0;
+    while (index < input.len) {
+        const num = readInt(u64, input, &index);
+        index += 1;
+        for (ranges) |range| {
+            if (num >= range.lower and num <= range.upper) {
+                fresh += 1;
+                break;
+            }
+        }
+    }
+    return fresh;
+}
+
+fn part2(alloc: Allocator, input: []const u8) !u64 {
+    var index: usize = 0;
+    const ranges = try createRanges(alloc, input, &index);
+    defer alloc.free(ranges);
+
+    var total: u64 = 0;
+    for (ranges) |range| {
+        total += range.upper - range.lower + 1;
+    }
+
+    return total;
 }
 
 test "part1" {
     const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
+    const input =
+        \\3-5
+        \\10-14
+        \\16-20
+        \\12-18
+        \\
+        \\1
+        \\5
+        \\8
+        \\11
+        \\17
+        \\32
+    ;
+    const expected = 3;
     const actual = try part1(alloc, input);
     try std.testing.expectEqual(expected, actual);
 }
 
 test "part2" {
     const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
+    const input =
+        \\3-5
+        \\10-14
+        \\16-20
+        \\12-18
+        \\
+        \\1
+        \\5
+        \\8
+        \\11
+        \\17
+        \\32
+    ;
+    const expected = 14;
     const actual = try part2(alloc, input);
     try std.testing.expectEqual(expected, actual);
 }
