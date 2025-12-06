05b9b3ac34ee01cf4e6e632ef348558f7c8f3e20
Zak-Temperton <zbtemperton@gmail.com>
2025-12-06 21:56:15 +1300

    day06

--- src/days/day06.zig
+++ src/days/day06.zig
@@ -12,7 +12,7 @@ pub fn run(alloc: std.mem.Allocator, stdout: *std.io.Writer) !void {
     var timer = try std.time.Timer.start();
     const p1 = try part1(alloc, buffer.data);
     const p1_time = timer.lap();
-    const p2 = try part2(alloc, buffer.data);
+    const p2 = part2(buffer.data);
     const p2_time = timer.read();
     try stdout.print(
         \\Day06:
@@ -25,30 +25,120 @@ pub fn run(alloc: std.mem.Allocator, stdout: *std.io.Writer) !void {
     });
 }
 
-fn part1(alloc: Allocator, input: []const u8) !u32 {
-    _ = alloc;
-    _ = input;
-    return 0;
+const Operation = enum {
+    add,
+    mul,
+};
+
+fn skipWhitespace(input: []const u8, index: *usize) void {
+    while (index.* < input.len and input[index.*] == ' ') {
+        index.* += 1;
+    }
 }
 
-fn part2(alloc: Allocator, input: []const u8) !u32 {
+fn part1(alloc: Allocator, input: []const u8) !u64 {
     _ = alloc;
-    _ = input;
-    return 0;
+    const line_length = std.mem.indexOf(u8, input, &.{'\n'}).? + 1;
+    const lines = (input.len + 1) / line_length;
+    const operations_offset = (lines - 1) * line_length;
+
+    var index: usize = 0;
+    var total: u64 = 0;
+
+    while (index < line_length - 1) {
+        const op: Operation = switch (input[operations_offset + index]) {
+            '+' => .add,
+            '*' => .mul,
+            else => unreachable,
+        };
+        var result: u64 = blk: {
+            var tmp = index;
+            skipWhitespace(input, &tmp);
+            break :blk readInt(u64, input, &tmp);
+        };
+        for (1..lines - 1) |l| {
+            var tmp = l * line_length + index;
+            skipWhitespace(input, &tmp);
+            const num = readInt(u64, input, &tmp);
+            switch (op) {
+                .add => result += num,
+                .mul => result *= num,
+            }
+        }
+        index += 1;
+        while (input[operations_offset + index] == ' ') index += 1;
+
+        total += result;
+    }
+    return total;
+}
+
+fn part2(input: []const u8) u64 {
+    const line_length = std.mem.indexOf(u8, input, &.{'\n'}).? + 1;
+    const lines = (input.len + 1) / line_length;
+    const operations_offset = (lines - 1) * line_length;
+
+    var index: usize = 0;
+    var total: u64 = 0;
+
+    while (index < line_length - 1) {
+        const op: Operation = switch (input[operations_offset + index]) {
+            '+' => .add,
+            '*' => .mul,
+            else => unreachable,
+        };
+        var result: u64 = 0;
+        for (0..lines - 1) |l| {
+            const c = input[line_length * l + index];
+            if (c != ' ') {
+                result *= 10;
+                result += c - '0';
+            }
+        }
+        index += 1;
+        while (index < line_length - 1 and input[operations_offset + index] == ' ') {
+            var num: u64 = 0;
+            for (0..lines - 1) |l| {
+                const c = input[line_length * l + index];
+                if (c != ' ') {
+                    num *= 10;
+                    num += c - '0';
+                }
+            }
+            if (num != 0) {
+                switch (op) {
+                    .add => result += num,
+                    .mul => result *= num,
+                }
+            }
+            index += 1;
+        }
+        total += result;
+    }
+    return total;
 }
 
 test "part1" {
     const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
+    const input =
+        \\123 328  51 64 
+        \\ 45 64  387 23 
+        \\  6 98  215 314
+        \\*   +   *   +  
+    ;
+    const expected = 4277556;
     const actual = try part1(alloc, input);
     try std.testing.expectEqual(expected, actual);
 }
 
 test "part2" {
-    const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
-    const actual = try part2(alloc, input);
+    const input =
+        \\123 328  51 64 
+        \\ 45 64  387 23 
+        \\  6 98  215 314
+        \\*   +   *   +  
+    ;
+    const expected = 3263827;
+    const actual = part2(input);
     try std.testing.expectEqual(expected, actual);
 }
