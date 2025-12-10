13b223333c4b593c4d7260fda629b913b02aea9c
Zak-Temperton <zbtemperton@gmail.com>
2025-12-10 18:24:55 +1300

    day08

--- src/days/day08.zig
+++ src/days/day08.zig
@@ -25,30 +25,163 @@ pub fn run(alloc: std.mem.Allocator, stdout: *std.io.Writer) !void {
     });
 }
 
+const JunctionBox = @Vector(3, u32);
+const Pair = struct {
+    a: usize,
+    b: usize,
+    dist: f32,
+
+    fn init(junction_boxes: []JunctionBox, a_index: usize, b_index: usize) Pair {
+        const a: @Vector(3, f32) = @floatFromInt(junction_boxes[a_index]);
+        const b: @Vector(3, f32) = @floatFromInt(junction_boxes[b_index]);
+        const ab = a - b;
+        const ab2 = ab * ab;
+        const dist = @sqrt(ab2[0] + ab2[1] + ab2[2]);
+        return .{
+            .a = a_index,
+            .b = b_index,
+            .dist = dist,
+        };
+    }
+
+    fn lessThan(_: void, left: Pair, right: Pair) bool {
+        return left.dist < right.dist;
+    }
+};
+const Node = struct {
+    walked: bool = false,
+    links: std.ArrayList(usize),
+    fn walkNode(node: *@This(), nodes: []?@This(), size: *u32) void {
+        if (node.walked) return;
+        node.walked = true;
+        size.* += 1;
+        for (node.links.items) |link| {
+            nodes[link].?.walkNode(nodes, size);
+        }
+    }
+};
+
 fn part1(alloc: Allocator, input: []const u8) !u32 {
-    _ = alloc;
-    _ = input;
-    return 0;
+    const num_boxes = 1000;
+    var junction_boxes = try alloc.alloc(JunctionBox, num_boxes);
+    defer alloc.free(junction_boxes);
+    var pairs = try alloc.alloc(Pair, num_boxes * (num_boxes - 1) / 2);
+    defer alloc.free(pairs);
+
+    var index: usize = 0;
+    var i: usize = 0;
+    while (index < input.len) : (i += 1) {
+        const x = readInt(u32, input, &index);
+        index += 1;
+        const y = readInt(u32, input, &index);
+        index += 1;
+        const z = readInt(u32, input, &index);
+        index += 1;
+        junction_boxes[i] = .{ x, y, z };
+    }
+
+    i = 0;
+    for (0..junction_boxes.len - 1) |a| {
+        for (a + 1..junction_boxes.len) |b| {
+            pairs[i] = Pair.init(junction_boxes, a, b);
+            i += 1;
+        }
+    }
+
+    std.mem.sort(Pair, pairs, {}, Pair.lessThan);
+
+    var nodes = try alloc.alloc(?Node, 1000);
+    defer {
+        for (0..nodes.len) |n| {
+            if (nodes[n] != null) {
+                nodes[n].?.links.deinit(alloc);
+            }
+        }
+        alloc.free(nodes);
+    }
+    @memset(nodes, null);
+
+    for (pairs[0..1000]) |pair| {
+        if (nodes[pair.a] == null) {
+            nodes[pair.a] = .{ .links = try std.ArrayList(usize).initCapacity(alloc, 8) };
+        }
+        try nodes[pair.a].?.links.append(alloc, pair.b);
+
+        if (nodes[pair.b] == null) {
+            nodes[pair.b] = .{ .links = try std.ArrayList(usize).initCapacity(alloc, 8) };
+        }
+        try nodes[pair.b].?.links.append(alloc, pair.a);
+    }
+
+    var max: [3]u32 = .{ 0, 0, 0 };
+
+    for (0..nodes.len) |node_i| {
+        if (nodes[node_i] != null) {
+            var size: u32 = 0;
+            nodes[node_i].?.walkNode(nodes, &size);
+
+            if (size >= max[0]) {
+                max[2] = max[1];
+                max[1] = max[0];
+                max[0] = size;
+            } else if (size >= max[1]) {
+                max[2] = max[1];
+                max[1] = size;
+            } else if (size > max[2]) {
+                max[2] = size;
+            }
+        }
+    }
+
+    return max[0] * max[1] * max[2];
 }
 
 fn part2(alloc: Allocator, input: []const u8) !u32 {
-    _ = alloc;
-    _ = input;
+    const num_boxes = 1000;
+    var junction_boxes = try alloc.alloc(JunctionBox, num_boxes);
+    defer alloc.free(junction_boxes);
+    var pairs = try alloc.alloc(Pair, num_boxes * (num_boxes - 1) / 2);
+    defer alloc.free(pairs);
+
+    var index: usize = 0;
+    var i: usize = 0;
+    while (index < input.len) : (i += 1) {
+        const x = readInt(u32, input, &index);
+        index += 1;
+        const y = readInt(u32, input, &index);
+        index += 1;
+        const z = readInt(u32, input, &index);
+        index += 1;
+        junction_boxes[i] = .{ x, y, z };
+    }
+
+    i = 0;
+    for (0..junction_boxes.len - 1) |a| {
+        for (a + 1..junction_boxes.len) |b| {
+            pairs[i] = Pair.init(junction_boxes, a, b);
+            i += 1;
+        }
+    }
+
+    std.mem.sort(Pair, pairs, {}, Pair.lessThan);
+
+    var nodes = try alloc.alloc(u1000, 1000);
+    defer alloc.free(nodes);
+    for (nodes, 0..) |*n, j| n.* = @as(u1000, 1) << @truncate(j);
+
+    i = 0;
+    for (pairs) |pair| {
+        i += 1;
+        nodes[pair.a] |= nodes[pair.b];
+        nodes[pair.b] = nodes[pair.a];
+        for (0..1000) |j| {
+            if (nodes[pair.a] >> @truncate(j) & 1 == 1) {
+                nodes[j] = nodes[pair.a];
+            }
+        }
+        if (nodes[pair.a] == std.math.maxInt(u1000)) {
+            return junction_boxes[pair.a][0] * junction_boxes[pair.b][0];
+        }
+    }
     return 0;
-}
-
-test "part1" {
-    const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
-    const actual = try part1(alloc, input);
-    try std.testing.expectEqual(expected, actual);
-}
-
-test "part2" {
-    const alloc = std.testing.allocator;
-    const input = "";
-    const expected = 0;
-    const actual = try part2(alloc, input);
-    try std.testing.expectEqual(expected, actual);
 }
